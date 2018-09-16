31ac101eadb80a4f53025d05d13a7084821b918e
Alexander Worton <alexander.worton@itv.com>
2018-09-16 14:41:39 +0100

    started migration to maybe selectedUrl

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,20 +1,45 @@
-module PhotoGroove exposing (..)
+module PhotoGroove exposing (Model, Msg(..), Photo, ThumbnailSize(..), getPhotoUrl, initialModel, main, photoArray, randomPhotoPicker, sizeToString, stringToSize, update, urlPrefix, view, viewSizeChooser, viewThumbnail)
 
 import Array exposing (Array)
 import Html exposing (..)
-import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Http
 import Random
 
-type alias Photo = { url: String }
-type alias Model = { photos: List Photo, selectedUrl: String, chosenSize: ThumbnailSize }
-type ThumbnailSize = Small | Medium | Large
-type Msg = SelectPhotoByUrl String | SurpriseMe | SetSize ThumbnailSize | SelectByIndex Int
 
-urlPrefix: String
-urlPrefix  = "http://elm-in-action.com/"
+type alias Photo =
+    { url : String }
 
-view: Model -> Html Msg
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : Maybe String
+    , loadingError : Maybe String
+    , chosenSize : ThumbnailSize
+    }
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+type Msg
+    = SelectPhotoByUrl String
+    | SurpriseMe
+    | SetSize ThumbnailSize
+    | SelectByIndex Int
+    | LoadPhotos (Result Http.Error String)
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
@@ -25,60 +50,88 @@ view model =
             (List.map (viewSizeChooser model.chosenSize) [ Small, Medium, Large ])
         , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
-        , img
-            [ class "large"
-            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
-            ] []
+
+        --, img
+        --    [ class "large"
+        --    , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+        --    ]
+        --    []
         ]
 
-viewThumbnail: String ->  Photo ->  Html Msg
-viewThumbnail selected thumbnail =
-    img [
-        src (urlPrefix ++ thumbnail.url)
-    ,   classList [ ( "selected", selected == thumbnail.url) ]
-    ,   onClick (SelectPhotoByUrl thumbnail.url)
-    ] []
 
-viewSizeChooser: ThumbnailSize -> ThumbnailSize -> Html Msg
+viewThumbnail : Maybe String -> Photo -> Html Msg
+viewThumbnail selected thumbnail =
+    img
+        [ src (urlPrefix ++ thumbnail.url)
+        , classList [ ( "selected", selected == Just thumbnail.url ) ]
+        , onClick (SelectPhotoByUrl thumbnail.url)
+        ]
+        []
+
+
+viewSizeChooser : ThumbnailSize -> ThumbnailSize -> Html Msg
 viewSizeChooser selectedSize size =
     label []
-        [ input [ name "size", type_ "radio", checked (selectedSize == size)
-                , onClick (SetSize size) ][]
+        [ input
+            [ name "size"
+            , type_ "radio"
+            , checked (selectedSize == size)
+            , onClick (SetSize size)
+            ]
+            []
         , text (sizeToString size)
         ]
 
-sizeToString: ThumbnailSize -> String
-sizeToString size = case size of
-    Small -> "small"
-    Medium -> "med"
-    _ -> "large"
 
-stringToSize: String -> ThumbnailSize
-stringToSize size = case size of
-    "small" -> Small
-    "med" -> Medium
-    _ -> Large
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
 
-initialModel: Model
+        Medium ->
+            "med"
+
+        _ ->
+            "large"
+
+
+stringToSize : String -> ThumbnailSize
+stringToSize size =
+    case size of
+        "small" ->
+            Small
+
+        "med" ->
+            Medium
+
+        _ ->
+            Large
+
+
+initialModel : Model
 initialModel =
     { photos =
-        [
-            { url = "1.jpeg" }
-        ,   { url = "2.jpeg" }
-        ,   { url = "3.jpeg" }
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
         ]
-        , selectedUrl = "1.jpeg"
-        , chosenSize = Medium
+    , selectedUrl = Just "1.jpeg"
+    , loadingError = Nothing
+    , chosenSize = Medium
     }
 
-photoArray: Array Photo
-photoArray = Array.fromList initialModel.photos
 
-update: Msg -> Model -> ( Model, Cmd Msg )
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SelectPhotoByUrl url ->
-            ( { model | selectedUrl = url }, Cmd.none )
+            ( { model | selectedUrl = Just url }, Cmd.none )
 
         SurpriseMe ->
             ( model, Random.generate SelectByIndex randomPhotoPicker )
@@ -87,21 +140,41 @@ update msg model =
             ( { model | chosenSize = size }, Cmd.none )
 
         SelectByIndex index ->
-            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
+            ( { model | selectedUrl = Just (getPhotoUrl index) }, Cmd.none )
+
+        LoadPhotos (Ok responseStr) ->
+            let
+                urls =
+                    String.split "," responseStr
+
+                photos =
+                    List.map Photo urls
+            in
+            ( { model | photos = photos }, Cmd.none )
+
+        LoadPhotos (Err _) ->
+            ( model, Cmd.none )
 
 
 getPhotoUrl : Int -> String
-getPhotoUrl index = case Array.get index photoArray of
-    Just photo -> photo.url
-    Nothing -> ""
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
 
-randomPhotoPicker: Random.Generator Int
-randomPhotoPicker = Random.int 0 (Array.length photoArray - 1)
+        Nothing ->
+            ""
+
+
+randomPhotoPicker : Random.Generator Int
+randomPhotoPicker =
+    Random.int 0 (Array.length photoArray - 1)
+
 
 main =
     Html.program
-    {   init = ( initialModel, Cmd.none )
-      , view = view
-      , update = update
-      , subscriptions = (\model -> Sub.none)
-    }
+        { init = ( initialModel, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \model -> Sub.none
+        }
